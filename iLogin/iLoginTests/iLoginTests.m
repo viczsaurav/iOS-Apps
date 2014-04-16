232ab2ebae6989eb0b1a00b9f039b6a86ce38768
Saurav Verma <viczsaurav@gmail.com>
2014-04-16 21:41:25 +0800

    iLogin App

---
+++ iLogin/iLoginTests/iLoginTests.m
@@ -0,0 +1,34 @@
+//
+//  iLoginTests.m
+//  iLoginTests
+//
+//  Created by student on 16/4/14.
+//  Copyright (c) 2014 ISS. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iLoginTests : XCTestCase
+
+@end
+
+@implementation iLoginTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
