11e8656cd3031fc7adff37934a82bd0436aa4ef0
Saurav Verma <viczsaurav@gmail.com>
2014-04-20 02:17:33 +0800

    MVC

---
+++ SupportEmailCall/SupportEmailCall/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SupportEmailCall
+//
+//  Created by Saurav on 4/20/14.
+//  Copyright (c) 2014 ISS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
