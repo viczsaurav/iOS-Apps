69e49447c8daefd13bdfd12e86a484e6f9df6b50
Saurav Verma <viczsaurav@gmail.com>
2014-04-19 23:30:57 +0800

    Account Calculator Done

--- SimpleCalculator/SimpleCalculator/ViewController.m
+++ SimpleCalculator/SimpleCalculator/ViewController.m
@@ -17,7 +17,22 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    textFieldPrincipal.text = @"10000";
+    textFieldNumberOfYears.text = [NSString stringWithFormat:@"%d", (int)sliderNUmberOfYears.value];
+    textFieldInterestRate.text = [NSString stringWithFormat:@"%.02f",sliderInterestRate.value];
+       
+    textFieldPrincipal.delegate = self;
+    textFieldNumberOfYears.delegate = self;
+    textFieldInterestRate.delegate = self;
+}
+
+- (BOOL) textFieldShouldReturn:(UITextField *)textField {
+    [textFieldPrincipal resignFirstResponder];
+    [textFieldNumberOfYears resignFirstResponder];
+    [textFieldInterestRate resignFirstResponder];
+    return YES;
 }
 
 - (void)didReceiveMemoryWarning
@@ -28,5 +43,32 @@
 
 //My Methods
 //Change With NUmber of Years Changed
+
+- (IBAction)sliderNumberOfYearsChanged:(id)sender {
+    int myNewValue = sliderNUmberOfYears.value;
+    textFieldNumberOfYears.text = [NSString stringWithFormat:@"%d",myNewValue];
+    [self displayAmount];
+}
+
+- (IBAction)sliderInterestRateChanged:(id)sender {
+    float myNewValue = sliderInterestRate.value;
+    textFieldInterestRate.text = [NSString stringWithFormat:@"%.02f",myNewValue];
+    [self displayAmount];
+}
+
+- (IBAction) displayAmount {
+    // Making Keyboard to go away
+    [textFieldPrincipal resignFirstResponder];
+    [textFieldNumberOfYears resignFirstResponder];
+    [textFieldInterestRate resignFirstResponder];
+    
+    float principalAmount = textFieldPrincipal.text.floatValue;
+    int numberOfYears = textFieldNumberOfYears.text.intValue;
+    float interestRate = textFieldInterestRate.text.floatValue/100;
+    float finalAmount = principalAmount * pow((1+ interestRate), numberOfYears);
+    NSLog(@"%.02f",finalAmount);
+    labelAmount.text = [NSString stringWithFormat:@"%.02f",finalAmount];
+    [textFieldPrincipal resignFirstResponder];
+}
 
 @end
